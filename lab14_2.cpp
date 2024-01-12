884984ce30e6a94eb08d1483f681b5f07212fce9
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-01-12 17:23:51 +0700

    Lab14.2 Complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,31 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+        for(int a = 0 ; a < N ; a++){
+            cout << "Row " << a+1 << ": ";
+            for (int b = 0; b < N ; b++) cin >> A[a][b];
+        }
+    }
+
+    void findLocalMax(const double input[][N], bool output[][N]){
+        for(int i = 0;i < N ;i++){
+            for(int j = 0 ; j < N ; j++){
+                if(i == 0 || j == 0 || j == N-1 || i == N-1){
+                    output[i][j] = 0;
+                }
+                else if( input[i][j] >= input[i-1][j] && input[i][j] >= input[i+1][j] && input[i][j]  >= input[i][j+1] && input[i][j] >= input[i][j-1] ){
+                    output[i][j] = 1;
+                }
+                else output[i][j] = 0;
+            }
+        }
+
+    }
+    void showMatrix(const bool metrix[][N]){
+        for(int i = 0 ; i < N ; i ++){
+            for(int j = 0; j < N ; j++) cout << metrix[i][j] << " ";
+            cout << endl;
+        }
+    }
